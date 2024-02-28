d8f281a57a9e98bfea566f8d73c9f9b19925b547
bass669 <111931670+bass669@users.noreply.github.com>
2024-02-28 15:29:25 -0300

    pdf
    prueba del pdf

--- jss/pdf.js
+++ jss/pdf.js
@@ -1,114 +1,312 @@
 function genPDF() {
   // Obtener los valores
-    const nombre = document.getElementById("nombre").value;
-    const turno = document.getElementById("turno").value;
-    const fecha = document.getElementById("fecha").value;
-    const elementos = document.querySelectorAll('[data-sufijo]');
-  //nuevo objeto jsPDF
-    const pdf = new jsPDF();
-    let yPos = 20;
-    pdf.setFillColor(31, 79, 120); // Relleno azul
-    pdf.rect(0, 0, pdf.internal.pageSize.width, 15, 'F');
-    pdf.setTextColor(255, 255, 255);
-    pdf.setFontSize(18);
-    pdf.text(8, 10, "Bitácora de turno");
-    pdf.setFillColor(255, 255, 255); //blanco
-    pdf.setTextColor(0, 0, 0); //negro
-    pdf.setFontSize(18);
+  const nombre = document.getElementById("nombre").value;
+  const turno = document.getElementById("turno").value;
+  const fecha = document.getElementById("fecha").value;
+  const elementos = document.querySelectorAll('[data-sufijo]');
+
+  // Nuevo objeto jsPDF
+  const pdf = new jsPDF();
+  let yPos = 20;
+
+  // Variables para comparar con los valores anteriores
+  let prevNombre = null, prevTurno = null, prevFecha = null;
+
+  // Configurar estilo de encabezado
+  pdf.setFillColor(31, 79, 120); // Relleno azul
+  pdf.rect(0, 0, pdf.internal.pageSize.width, 15, 'F');
+  pdf.setTextColor(255, 255, 255);
+  pdf.setFontSize(18);
+  pdf.text(8, 10, "Bitácora de turno");
+
+  // Configurar estilo de contenido
+  pdf.setFillColor(255, 255, 255); // Blanco
+  pdf.setTextColor(0, 0, 0); // Negro
+  pdf.setFontSize(18);
+
   // Recorrer elementos y obtener valores
-    elementos.forEach(function (elemento) {
-  // Reiniciar acumuladorInfo
+  elementos.forEach(function (elemento, index) {
+    // Reiniciar acumuladorInfo
     let acumuladorInfo = "";
-  // Obtener el sufijo y valores
+
+    // Obtener el sufijo y valores
     const sufijo = elemento.dataset.sufijo;
     const area = document.getElementById(`area_${sufijo}`).value;
     const inicio = document.getElementById(`inicio_${sufijo}`).value;
     const termino = document.getElementById(`termino_${sufijo}`).value;
     const difTiempo = document.getElementById(`difTiempo_${sufijo}`).value;
     const observaciones = document.getElementById(`observaciones_${sufijo}`).value;
-  // valor de subárea
+
+    // Valor de subárea
     let subarea;
     if (area === "Chapas") {
       subarea = document.getElementById(`subareaChapas_${sufijo}`).value;
-  } else if (area === "Tableros") {
+    } else if (area === "Tableros") {
       subarea = document.getElementById(`subareaTableros_${sufijo}`).value;
-  } else if (area === "Terminacion") {
+    } else if (area === "Terminacion") {
       subarea = document.getElementById(`subareaTerminacion_${sufijo}`).value;
-  } else {
-      subarea = "";}
-  // Acumular la info
-    acumuladorInfo += `\n
-  Nombre: ${nombre}\n
-  ${turno}\n
-  Fecha actual: ${fecha}\n
-  Área: ${area}\n
-  Equipo: ${subarea}\n
-  Observaciones: \n \n ${observaciones}\n
-  Hora de Inicio: ${inicio}\n
-  Hora de Termino: ${termino}\n
-  Diferencia de Tiempo: ${difTiempo}\n`;
-  if (yPos + 105 > pdf.internal.pageSize.height) {
-    pdf.addPage(); yPos = 20;}
-  // Agregar la información
-    pdf.text(8, yPos, acumuladorInfo);});
-  // Usar el arreglo bases
-  const bases = window.bases;
-  // Comprobar si hay algún elemento
-  if (bases.length > 0) {
-  // guardar las promesas
-    const promises = [];
-  // Recorrer el arreglo
-    for (const base64 of bases) {
-  // Crear una promesa para cada imagen
-      const promise = new Promise((resolve, reject) => {
-  // obtener su tamaño real
-      const img = new Image();
-      img.src = base64;
-  // Esperar que la imagen esté cargada
-      img.onload = function () {
-  // Resolver la promesa con la imagen
-      resolve(img);};
-  // Manejar el caso de error
-      img.onerror = function () {
-      reject("Error al cargar la imagen");};});
-  // Añadir la promesa al arreglo
-      promises.push(promise);}
-  // Esperar que las promesas se resuelvan
-    Promise.all(promises)
-      .then((images) => {
-  // Variable para indicar si es la primera imagen
-      let first = true;
-  // Recorrer el arreglo de imágenes
-    for (const img of images) {
-    // Obtener el tamaño
-      const iWidth = img.naturalWidth;
-      const iHeight = img.naturalHeight;
-    // Reducir el tamaño 
-      const width = iWidth / 20;
-      const height = iHeight / 20;
-    // Calcular las coordenadas x e y
-      const x = (pdf.internal.pageSize.width - width) / 2;
-      const y = (pdf.internal.pageSize.height - height) / 2;
-    // Si es la primera imagen, añadir una página en blanco antes
-    if (first) {
-      pdf.addPage();
-    }
-    // Añadir la imagen con las coordenadas
-    pdf.addImage(
-      img.src,"JPEG",x,y,width,height);
-      // Cambiar el valor de first a false
-      first = false;  
-    // Si no es la primera imagen, añadir una página después
-    if (!first) {
-      pdf.addPage(
-        img.src,"JPEG",x,y,width,height);
+    } else {
+      subarea = "";
     }
 
+    // Construir una cadena con los valores actuales
+    const currentValues = `${nombre}${turno}${fecha}${area}${subarea}${observaciones}${inicio}${termino}${difTiempo}`;
+
+    // Comparar con los valores anteriores
+    if (
+      currentValues !== prevNombre ||
+      currentValues !== prevTurno ||
+      currentValues !== prevFecha
+    ) {
+      // Añadir nueva página solo si los valores son diferentes
+      if (index > 0) {
+        pdf.addPage();
+        yPos = 20;
+      }
     }
-    // Guardar el PDF
-    pdf.save("Bitácora "+nombre+" "+fecha+".pdf");})
-    .catch((error) => {
-      alert(error);});
+
+    // Dividir las observaciones en líneas
+    const lines = observaciones.split('\n');
+
+    // Acumular información
+    acumuladorInfo += `\nNombre: ${nombre}\n${turno}\nFecha actual: ${fecha}\nÁrea: ${area}\nEquipo: ${subarea}\n`;
+
+    // Añadir líneas de observaciones
+    acumuladorInfo += `Observaciones: ${observaciones}\n`;
+
+    // Añadir el resto de la información
+    acumuladorInfo += `Hora de Inicio: ${inicio}\nHora de Termino: ${termino}\nDiferencia de Tiempo: ${difTiempo}\n`;
+
+    // Agregar la información
+    pdf.text(8, yPos, acumuladorInfo);
+
+
+
+    // Actualizar los valores anteriores
+    prevNombre = currentValues;
+    prevTurno = currentValues;
+    prevFecha = currentValues;
+  });
+
+  // Usar el arreglo bases
+  const bases = window.bases;
+
+  // Comprobar si hay elementos
+  if (bases.length > 0) {
+    // Guardar las promesas
+    const promises = [];
+
+    // Recorrer el arreglo de bases
+    for (const base64 of bases) {
+      // Crear una promesa para cada imagen
+      const promise = new Promise((resolve, reject) => {
+        // Obtener el tamaño de la imagen
+        const img = new Image();
+        img.src = base64;
+
+        // Esperar a que la imagen esté cargada
+        img.onload = function () {
+          // Resolver la promesa con la imagen
+          resolve(img);
+        };
+
+        // Manejar el caso de error
+        img.onerror = function () {
+          reject("Error al cargar la imagen");
+        };
+      });
+
+      // Añadir la promesa al arreglo
+      promises.push(promise);
+    }
+
+    // Esperar a que todas las promesas se resuelvan
+    Promise.all(promises)
+      .then((images) => {
+        // Variable para indicar si es la primera imagen
+        let first = true;
+
+        // Recorrer el arreglo de imágenes
+        for (const img of images) {
+          // Obtener tamaño de la imagen
+          const iWidth = img.naturalWidth;
+          const iHeight = img.naturalHeight;
+
+          // Reducir tamaño de la imagen
+          const width = iWidth / 20;
+          const height = iHeight / 20;
+
+          // Calcular coordenadas
+          const x = (pdf.internal.pageSize.width - width) / 2;
+          const y = (pdf.internal.pageSize.height - height) / 2;
+
+          // Si es la primera imagen, añadir una página
+          if (first) {
+            pdf.addPage();
+          }
+
+          // Añadir la imagen con las coordenadas
+          pdf.addImage(img.src, "JPEG", x, y, width, height);
+
+          // Cambiar el valor de 'first'
+          first = false;
+        }
+
+        // Guardar el PDF
+        pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
+      })
+      .catch((error) => {
+        alert(error);
+      });
   } else {
-    pdf.save("Bitácora "+nombre+" "+fecha+".pdf");}}
-  
+    pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
+  }
+}
+
+
+
+
+// function genPDF() {
+//   // Obtener los valores
+//   const nombre = document.getElementById("nombre").value;
+//   const turno = document.getElementById("turno").value;
+//   const fecha = document.getElementById("fecha").value;
+//   const elementos = document.querySelectorAll('[data-sufijo]');
+
+//   // Nuevo objeto jsPDF
+//   const pdf = new jsPDF();
+//   let yPos = 20;
+
+//   // Configurar estilo de encabezado
+//   pdf.setFillColor(31, 79, 120); // Relleno azul
+//   pdf.rect(0, 0, pdf.internal.pageSize.width, 15, 'F');
+//   pdf.setTextColor(255, 255, 255);
+//   pdf.setFontSize(18);
+//   pdf.text(8, 10, "Bitácora de turno");
+
+//   // Configurar estilo de contenido
+//   pdf.setFillColor(255, 255, 255); // Blanco
+//   pdf.setTextColor(0, 0, 0); // Negro
+//   pdf.setFontSize(18);
+
+//   // Recorrer elementos y obtener valores
+//   elementos.forEach(function (elemento, index) {
+//     // Reiniciar acumuladorInfo
+//     let acumuladorInfo = "";
+
+//     // Obtener el sufijo y valores
+//     const sufijo = elemento.dataset.sufijo;
+//     const area = document.getElementById(`area_${sufijo}`).value;
+//     const inicio = document.getElementById(`inicio_${sufijo}`).value;
+//     const termino = document.getElementById(`termino_${sufijo}`).value;
+//     const difTiempo = document.getElementById(`difTiempo_${sufijo}`).value;
+//     const observaciones = document.getElementById(`observaciones_${sufijo}`).value;
+
+//     // Valor de subárea
+//     let subarea;
+//     if (area === "Chapas") {
+//       subarea = document.getElementById(`subareaChapas_${sufijo}`).value;
+//     } else if (area === "Tableros") {
+//       subarea = document.getElementById(`subareaTableros_${sufijo}`).value;
+//     } else if (area === "Terminacion") {
+//       subarea = document.getElementById(`subareaTerminacion_${sufijo}`).value;
+//     } else {
+//       subarea = "";
+//     }
+
+//     // Acumular info
+//     acumuladorInfo += `\n
+//       Nombre: ${nombre}\n
+//       ${turno}\n
+//       Fecha actual: ${fecha}\n
+//       Área: ${area}\n
+//       Equipo: ${subarea}\n
+//       Observaciones: \n \n ${observaciones}\n
+//       Hora de Inicio: ${inicio}\n
+//       Hora de Termino: ${termino}\n
+//       Diferencia de Tiempo: ${difTiempo}\n`;
+
+//     // Agregar la información
+//     pdf.text(8, yPos, acumuladorInfo);
+
+//     // Si hay más elementos, añadir una nueva página
+//     if (index < elementos.length - 1) {
+//       pdf.addPage();
+//       yPos = 20;
+//     }
+//   });
+
+//   // Usar el arreglo bases
+//   const bases = window.bases;
+
+//   // Comprobar si hay elementos
+//   if (bases.length > 0) {
+//     // Guardar las promesas
+//     const promises = [];
+
+//     // Recorrer el arreglo de bases
+//     for (const base64 of bases) {
+//       // Crear una promesa para cada imagen
+//       const promise = new Promise((resolve, reject) => {
+//         // Obtener el tamaño de la imagen
+//         const img = new Image();
+//         img.src = base64;
+
+//         // Esperar a que la imagen esté cargada
+//         img.onload = function () {
+//           // Resolver la promesa con la imagen
+//           resolve(img);
+//         };
+
+//         // Manejar el caso de error
+//         img.onerror = function () {
+//           reject("Error al cargar la imagen");
+//         };
+//       });
+
+//       // Añadir la promesa al arreglo
+//       promises.push(promise);
+//     }
+
+//     // Esperar a que todas las promesas se resuelvan
+//     Promise.all(promises)
+//       .then((images) => {
+//         // Variable para indicar si es la primera imagen
+//         let first = true;
+
+//         // Recorrer el arreglo de imágenes
+//         for (const img of images) {
+//           // Obtener tamaño de la imagen
+//           const iWidth = img.naturalWidth;
+//           const iHeight = img.naturalHeight;
+
+//           // Reducir tamaño de la imagen
+//           const width = iWidth / 20;
+//           const height = iHeight / 20;
+
+//           // Calcular coordenadas
+//           const x = (pdf.internal.pageSize.width - width) / 2;
+//           const y = (pdf.internal.pageSize.height - height) / 2;
+
+//           // Si es la primera imagen, añadir una página
+//           if (first) {
+//             pdf.addPage();
+//           }
+
+//           // Añadir la imagen con las coordenadas
+//           pdf.addImage(img.src, "JPEG", x, y, width, height);
+
+//           // Cambiar el valor de 'first'
+//           first = false;
+//         }
+
+//         // Guardar el PDF
+//         pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
+//       })
+//       .catch((error) => {
+//         alert(error);
+//       });
+//   } else {
+//     pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
+//   }
+// }
