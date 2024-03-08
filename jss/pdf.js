4eb0d50f57374d04f0841ea3189433e7a0bc309f
bass669 <111931670+bass669@users.noreply.github.com>
2024-03-08 12:26:11 -0300

    Update pdf.js

--- jss/pdf.js
+++ jss/pdf.js
@@ -180,6 +180,13 @@
 
 
 
+
+
+
+
+
+
+
 function genPDF() {
   let prevNombre = null;
   let prevTurno = null;
@@ -251,7 +258,6 @@ function genPDF() {
     prevTurno = currentValues;
     prevFecha = currentValues;
 
-    yPosTexto += 10; // Añadir espacio después de cada bloque de texto
   });
 
   const bases = window.bases;
@@ -277,12 +283,10 @@ function genPDF() {
         images.forEach(img => {
           const iWidth = img.naturalWidth;
           const iHeight = img.naturalHeight;
-          const scale = (pdf.internal.pageSize.width - 20) / iWidth; // Escala para ajustar la imagen al ancho de la página
-          const width = iWidth * scale;
-          const height = iHeight * scale;
+          const width = iWidth / 25;
+          const height = iHeight / 25;
   
           // Asegúrate de que la imagen cabe en la página, si no, añade una nueva página
-          pdf.addPage();
           if (yPosImagenes + height > pdf.internal.pageSize.height) {
             pdf.addPage();
             yPosImagenes = 20; // Restablece la posición Y para la nueva página
@@ -290,8 +294,7 @@ function genPDF() {
   
           yPosImagenes += 10; // Espacio antes de la imagen
   
-          // Añade la imagen con una calidad reducida
-          pdf.addImage(img.src, "JPEG", 10, yPosImagenes, width, height, '', 'FAST');
+          pdf.addImage(img.src, "JPEG", 10, yPosImagenes, width, height);
   
           yPosImagenes += height + 10; // Espacio después de la imagen
         });
@@ -305,157 +308,7 @@ function genPDF() {
   } else {
     pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
   }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function genPDF() {
-//   let prevNombre = null;
-//   let prevTurno = null;
-//   let prevFecha = null;
-
-//   const nombre = document.getElementById("nombre").value;
-//   const turno = document.getElementById("turno").value;
-//   const fecha = document.getElementById("fecha").value;
-//   const elementos = document.querySelectorAll('[data-sufijo]');
-//   const pdf = new jsPDF();
-
-//   let yPosTexto = 20;
-//   let yPosImagenes = 20;
-
-//   // Configurar estilo de encabezado
-//   pdf.setFillColor(31, 79, 120); // Relleno azul
-//   pdf.rect(0, 0, pdf.internal.pageSize.width, 15, 'F');
-//   pdf.setTextColor(255, 255, 255);
-//   pdf.setFontSize(18);
-//   pdf.text(8, 10, "Bitácora de turno");
-
-//   // Configurar estilo de contenido
-//   pdf.setFillColor(255, 255, 255); // Blanco
-//   pdf.setTextColor(0, 0, 0); // Negro
-//   pdf.setFontSize(18);
-
-//   elementos.forEach(function (elemento, index) {
-//     let acumuladorInfo = "";
-//     const sufijo = elemento.dataset.sufijo;
-//     const area = document.getElementById(`area_${sufijo}`).value;
-//     const inicio = document.getElementById(`inicio_${sufijo}`).value;
-//     const termino = document.getElementById(`termino_${sufijo}`).value;
-//     const difTiempo = document.getElementById(`difTiempo_${sufijo}`).value;
-//     const observaciones = document.getElementById(`observaciones_${sufijo}`).value;
-
-//     let subarea;
-//     if (area === "Chapas") {
-//       subarea = document.getElementById(`subareaChapas_${sufijo}`).value;
-//     } else if (area === "Tableros") {
-//       subarea = document.getElementById(`subareaTableros_${sufijo}`).value;
-//     } else if (area === "Terminacion") {
-//       subarea = document.getElementById(`subareaTerminacion_${sufijo}`).value;
-//     } else {
-//       subarea = "";
-//     }
-
-//     const currentValues = `${nombre}${turno}${fecha}${area}${subarea}${observaciones}${inicio}${termino}${difTiempo}`;
-
-//     if (
-//       currentValues !== prevNombre ||
-//       currentValues !== prevTurno ||
-//       currentValues !== prevFecha
-//     ) {
-//       if (index > 0) {
-//         pdf.addPage();
-//         yPosTexto = 20;
-//       }
-//     }
-
-//     const lines = observaciones.split('\n');
-
-//     acumuladorInfo += `\nNombre: ${nombre}\n \n ${turno}\n \n Fecha actual: ${fecha}\n \n Área: ${area}\n \n Equipo: ${subarea}\n \n `;
-//     acumuladorInfo += `Observaciones:\n ${observaciones}\n \n `;
-//     acumuladorInfo += `Hora de Inicio: ${inicio}\n \n Hora de Termino: ${termino}\n \n Diferencia de Tiempo: ${difTiempo}\n \n `;
-
-//     pdf.text(8, yPosTexto, acumuladorInfo);
-
-//     prevNombre = currentValues;
-//     prevTurno = currentValues;
-//     prevFecha = currentValues;
-
-//   });
-
-//   const bases = window.bases;
-
-//   if (bases.length > 0) {
-//     const promises = bases.map(src => {
-//       return new Promise((resolve, reject) => {
-//         const img = new Image();
-//         img.src = src;
-  
-//         img.onload = function () {
-//           resolve(img);
-//         };
-  
-//         img.onerror = function () {
-//           reject("Error al cargar la imagen");
-//         };
-//       });
-//     });
-  
-//     Promise.all(promises)
-//       .then(images => {
-//         images.forEach(img => {
-//           const iWidth = img.naturalWidth;
-//           const iHeight = img.naturalHeight;
-//           const width = iWidth / 25;
-//           const height = iHeight / 25;
-  
-//           // Asegúrate de que la imagen cabe en la página, si no, añade una nueva página
-//           if (yPosImagenes + height > pdf.internal.pageSize.height) {
-//             pdf.addPage();
-//             yPosImagenes = 20; // Restablece la posición Y para la nueva página
-//           }
-  
-//           yPosImagenes += 10; // Espacio antes de la imagen
-  
-//           pdf.addImage(img.src, "JPEG", 10, yPosImagenes, width, height);
-  
-//           yPosImagenes += height + 10; // Espacio después de la imagen
-//         });
-  
-//         pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
-//       })
-//       .catch(error => {
-//         console.error(error);
-//         alert("Hubo un error al cargar las imágenes.");
-//       });
-//   } else {
-//     pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
-//   }
-// }  
+}  
 
 
 
