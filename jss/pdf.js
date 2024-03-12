fb5289d5dbb58b1164ea0aeeca08063450914c97
bass669 <111931670+bass669@users.noreply.github.com>
2024-03-12 12:17:32 -0300

    Update pdf.js

--- jss/pdf.js
+++ jss/pdf.js
@@ -229,24 +229,20 @@
 
 
 function genPDF() {
-  let prevNombre = null;
-  let prevTurno = null;
-  let prevFecha = null;
   const nombre = document.getElementById("nombre").value;
   const turno = document.getElementById("turno").value;
   const fecha = document.getElementById("fecha").value;
   const elementos = document.querySelectorAll('[data-sufijo]');
   const pdf = new jsPDF();
   let yPosTexto = 20;
-  let yPosImagenes = 20;
+  let primeraImagenAgregada = false; // Nuevo indicador para la primera imagen
 
-  // Configurar encabezado
+  // Configurar encabezado y contenido
   pdf.setFillColor(31, 79, 120); // Relleno azul
   pdf.rect(0, 0, pdf.internal.pageSize.width, 15, 'F');
   pdf.setTextColor(255, 255, 255);
   pdf.setFontSize(18);
   pdf.text(8, 10, "Bitácora de turno");
-  // Configurar estilo de contenido
   pdf.setFillColor(255, 255, 255); // Blanco
   pdf.setTextColor(0, 0, 0); // Negro
   pdf.setFontSize(18);
@@ -270,67 +266,58 @@ function genPDF() {
       subarea = "";
     }
 
-    const currentValues = `${nombre}${turno}${fecha}${area}${subarea}${observaciones}${inicio}${termino}${difTiempo}`;
-
-    if (
-      currentValues !== prevNombre ||
-      currentValues !== prevTurno ||
-      currentValues !== prevFecha
-    ) {
-      if (index > 0) {
-        pdf.addPage();
-        yPosTexto = 20;
-      }
-    }
-
-    const lines = observaciones.split('\n');
     acumuladorInfo += `\nNombre: ${nombre}\n \n ${turno}\n \n Fecha actual: ${fecha}\n \n Área: ${area}\n \n Equipo: ${subarea}\n \n `;
     acumuladorInfo += `Observaciones:\n ${observaciones}\n \n `;
     acumuladorInfo += `Hora de Inicio: ${inicio}\n \n Hora de Termino: ${termino}\n \n Diferencia de Tiempo: ${difTiempo}\n \n `;
 
     pdf.text(8, yPosTexto, acumuladorInfo);
-    prevNombre = currentValues;
-    prevTurno = currentValues;
-    prevFecha = currentValues;
+    yPosTexto += 10; // Asegurar espacio entre textos
   });
 
   const bases = window.bases;
 
   if (bases.length > 0) {
-    const promises = bases.map(src => {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.src = src;
-        img.onload = function () {
-          resolve(img);
-          // Se movió la adición de página nueva aquí para evitar páginas en blanco
-          if (yPosImagenes + img.naturalHeight / 10 > pdf.internal.pageSize.height) {
-            pdf.addPage();
-            yPosImagenes = 20; // Restablece la posición Y para la nueva página
-          }
-          yPosImagenes += 10; // Espacio antes de la imagen
-          pdf.addImage(img.src, "JPEG", 10, yPosImagenes, img.naturalWidth / 10, img.naturalHeight / 10);
-          yPosImagenes += img.naturalHeight / 10 + 10; // Espacio después de la imagen
-        };
+    bases.forEach(src => {
+      const img = new Image();
+      img.src = src;
+      img.onload = function () {
+        // Si es la primera imagen, añadir una nueva página
+        if (!primeraImagenAgregada) {
+          pdf.addPage();
+          primeraImagenAgregada = true; // Actualizar el indicador
+        }
 
-        img.onerror = function () {
-          reject("Error al cargar la imagen");
-        };
-      });
+        // Asegúrate de que la imagen cabe en la página, si no, añade una nueva página
+        if (yPosTexto + this.naturalHeight / 10 > pdf.internal.pageSize.height) {
+          pdf.addPage();
+          yPosTexto = 20; // Restablece la posición Y para la nueva página
+        }
+
+        yPosTexto += 10; // Espacio antes de la imagen
+        pdf.addImage(this.src, "JPEG", 10, yPosTexto, this.naturalWidth / 10, this.naturalHeight / 10);
+        yPosTexto += this.naturalHeight / 10 + 10; // Espacio después de la imagen
+      };
+
+      img.onerror = function () {
+        console.error("Error al cargar la imagen");
+      };
     });
-
-    Promise.all(promises)
-      .then(() => {
-        pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
-      })
-      .catch(error => {
-        console.error(error);
-        alert("Hubo un error al cargar las imágenes.");
-      });
-  } else {
-    pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
   }
+
+  // Guardar el PDF una vez que todas las imágenes se hayan cargado
+  Promise.all(bases.map(img => new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = img;
+    image.onload = resolve;
+    image.onerror = reject;
+  }))).then(() => {
+    pdf.save("Bitácora " + nombre + " " + fecha + ".pdf");
+  }).catch(error => {
+    console.error(error);
+    alert("Hubo un error al cargar las imágenes.");
+  });
 }
+
 
 
 
