3f26ba0d43bb2459943b490c9dec92fa7427acd4
bass669 <111931670+bass669@users.noreply.github.com>
2024-03-19 08:55:20 -0300

    Update pdf.js

--- jss/pdf.js
+++ jss/pdf.js
@@ -12,7 +12,7 @@ function genPDF() {
   pdf.setFillColor(31, 79, 120); // Relleno azul
   pdf.rect(0, 0, pdf.internal.pageSize.width, 25, 'F'); // Largo del encabezado
   pdf.setTextColor(255, 255, 255);
-  pdf.setFontSize(28); // Tamaño del texto
+  pdf.setFontSize(26); // Tamaño del texto
   pdf.text(8, 15, "Bitácora de turno " + Bitácora);
   pdf.setFillColor(255, 255, 255); // Blanco
   pdf.setTextColor(0, 0, 0); // Negro
