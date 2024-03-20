588f56063327e8af69f5525b42363dddfdc9fe39
bass669 <111931670+bass669@users.noreply.github.com>
2024-03-20 08:24:58 -0300

    Update base64.js

--- jss/base64.js
+++ jss/base64.js
@@ -1,21 +1,21 @@
-// Este módulo manejará todo lo relacionado con la fuente personalizada
-// ES6 Modules
-import jsPDF from 'jspdf';
-// CommonJS
-const jsPDF = require('jspdf');
+// // Este módulo manejará todo lo relacionado con la fuente personalizada
+// // ES6 Modules
+// import jsPDF from 'jspdf';
+// // CommonJS
+// const jsPDF = require('jspdf');
 
 
-// Función para agregar la fuente al VFS de jsPDF
-export function agregarFuenteAlVFS(pdf, miFuenteBase64) {
-    pdf.addFileToVFS('miFuente.ttf', miFuenteBase64);
-}
+// // Función para agregar la fuente al VFS de jsPDF
+// export function agregarFuenteAlVFS(pdf, miFuenteBase64) {
+//     pdf.addFileToVFS('miFuente.ttf', miFuenteBase64);
+// }
 
-// Función para registrar la fuente en jsPDF
-export function registrarFuente(pdf, nombreFuente) {
-  pdf.addFont('miFuente.ttf', nombreFuente, 'normal');
-}
+// // Función para registrar la fuente en jsPDF
+// export function registrarFuente(pdf, nombreFuente) {
+//   pdf.addFont('miFuente.ttf', nombreFuente, 'normal');
+// }
 
-// Función para establecer la fuente en el documento
-export function establecerFuente(pdf, nombreFuente) {
-  pdf.setFont(nombreFuente);
-}
+// // Función para establecer la fuente en el documento
+// export function establecerFuente(pdf, nombreFuente) {
+//   pdf.setFont(nombreFuente);
+// }
